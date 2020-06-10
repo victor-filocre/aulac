734de850efeef9d8abb5bc87f5c6c4cd39be39e6
victor-filocre <filocrevictor@gmail.com>
2020-06-10 17:51:49 -0300

    Aula3 do curso onde ensina o \n e a fazer comentarios com // e /* */

---
+++ aula3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+    // Fazer um comentario              - Apenas uma linha
+    /*
+                                        - Pode usar
+                                        - varias linhas
+
+    */ 
+    printf ("Arroz branco.\n");         // (\n) informa ao compilador para pular uma linha.
+
+    return 0;
+}
