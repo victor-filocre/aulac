123848bfcef6756ab147a3b217ba1ca3e0ea0a7e
victor-filocre <filocrevictor@gmail.com>
2020-06-10 18:32:58 -0300

    Aula5 do curso, onde ensina inserir mais de uma variavel no comando printf!

---
+++ aula5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main ()
+{
+    int minhaIdade;                         // variavel de valor inteiro (-2 -1 0 1 2 ..)
+
+    minhaIdade = 21;                        // valor da variavel
+
+    int maeIdade = 61;
+
+    int paiIdade = 71;
+
+    printf (" Minha idade = %i\n Pai idade = %i\n Mae idade = %i\n",
+             minhaIdade,paiIdade,maeIdade );         // %i inserir em textos variavel de valor inteiro
+
+    return 0;
+}
