4d0d021e111aa6fd8cc984a1b5e8fcca5eacd9b1
victor-filocre <filocrevictor@gmail.com>
2020-06-10 18:14:28 -0300

    Aula4 do curso, onde ensina o conceito de variavel int e como inseri-la em mensagem no formato printf.

---
+++ aula4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+    int minhaIdade;                         // variavel de valor inteiro (-2 -1 0 1 2 ..)
+
+    minhaIdade = 21;                        // valor da variavel
+
+    printf ("Minha idade e = %i.\n", minhaIdade);         // %i inserir em textos variavel de valor inteiro
+
+    return 0;
+}
