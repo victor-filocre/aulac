82cae56e4ffc18b7127f8029d5e221630c6f83d8
victor-filocre <filocrevictor@gmail.com>
2020-06-11 12:02:58 -0300

    Aula14 - Como utilizar if else

---
+++ aula14.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main ()
+{
+    int idade;
+
+    printf ("Digite sua idade:\n");
+    scanf ("%i",&idade);
+
+    if ( idade < 18 )                   // se
+    {
+        printf("Bebidas nao liberadas\n");
+    }
+    else                        // se nao
+    {
+        printf("Bebidas liberadas\n");
+    }
+   
+    return 0;
+}
