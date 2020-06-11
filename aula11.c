eb95049541845c245c621160d9c8434da2d6ec93
victor-filocre <filocrevictor@gmail.com>
2020-06-11 11:00:46 -0300

    Aula11 - Como utilizar o While

---
+++ aula11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int contador = 1;
+    
+    while (contador <= 5)               // enquanto o contador for menor ou igual a 5 executar a funcao no bloco de comando
+
+    {
+        printf ("%i\n", contador);
+        ++contador;             // contador = contador +1
+    }
+
+                    //  = definindo      == comparando
+    return 0;
+}
