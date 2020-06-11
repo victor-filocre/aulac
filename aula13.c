f60d28502f9a4c05e6886e1728392243315baf9d
victor-filocre <filocrevictor@gmail.com>
2020-06-11 11:52:22 -0300

    Aula13 - Como utilizar do while

---
+++ aula13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+    int i = 0;
+
+/*    while (i != 0){         // verifica primeiro depois executa
+
+        printf("Teste\n");
+    }
+*/
+    do {                        // executa o programa uma vez depois verifica a condicao
+        printf ("Teste2\n");
+    }   while (i!=0);
+
+
+    return 0;
+}
