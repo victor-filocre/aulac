4a9161e4339839a20288fc7fdeab4a64028d1f1f
victor-filocre <filocrevictor@gmail.com>
2020-06-13 12:21:31 -0300

    nothing

--- aula14.c
+++ aula14.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-int main ()
+int main()
 {
     int idade;
 
-    printf ("Digite sua idade:\n");
-    scanf ("%i",&idade);
+    printf("Digite sua idade:\n");
+    scanf("%i", &idade);
 
-    if ( idade < 18 )                   // se
+    if (idade < 18) // se
     {
         printf("Bebidas nao liberadas\n");
     }
-    else                        // se nao
+    else // se nao
     {
         printf("Bebidas liberadas\n");
     }
-   
+
     return 0;
 }
